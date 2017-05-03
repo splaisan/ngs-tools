2add839cb696a4b536e9db2bf263cd2d7bdb8fc3
Stephane Plaisance <stephane.plaisance@vib.be>
2017-05-03 22:22:54 +0200

    edits

--- fasta-tools/fasta2chromsizes.pl
+++ fasta-tools/fasta2chromsizes.pl
@@ -4,6 +4,7 @@
 # Create a table of chromosome lengths from a multifasta file
 #
 # Stephane Plaisance (VIB-NC+BITS) 2015/11/11; v1.00b
+# 1.1 allowing archived input
 # print to STDOUT to allow pipe
 #
 # visit our Git: https://github.com/BITS-VIB
@@ -31,24 +32,54 @@ defined($opt_h) && die $usage . "\n";
 
 # handle IO
 my $inpath = dirname($infile);
-my @sufx = ( ".fa", ".fasta", ".fsa" );
+my @sufx = (
+	".fa", ".fasta", ".fsa", ".fna",
+	".fa.gz", ".fasta.gz", ".fsa.gz", ".fna.gz",
+	".fa.zip", ".fasta.zip", ".fsa.zip", ".fna.zip",
+	".fa.bz2", ".fasta.bz2", ".fsa.bz2", ".fna.bz2", 
+	);
 my $name = basename( $infile, @sufx );
-#my $outpath = $inpath."/".$name.".chrom.sizes";
 
-# create output file
-#open(OUT, ">".$outpath) || die "Error: cannot create output file :$!\n";
-my $seqIO = Bio::SeqIO->new(-file=>$infile, -format=>"Fasta");
+# bioperl filehandles
+my $in = OpenArchiveFile($infile);
+my $totlen = 0;
 
 # loop through sequences and search motifs
-while ( my $seq = $seqIO->next_seq() ) {
+while ( my $seq = $in->next_seq() ) {
 	my $title = $seq->id;
 	print STDERR "# processing $title\n";
 	my $chrlen = $seq->length;
 	# test if long enough
 	$chrlen >= $minlen || next;
 	print STDOUT $title."\t".$chrlen."\n";
+	$totlen+=$chrlen;
 	}
+
+print STDOUT "# Total length (filtered)\t".$totlen."\n";
 
 # end
 #close OUT;
 exit 0;
+
+#### Subs ####
+sub OpenArchiveFile {
+    my $infile = shift;
+    my $FH;
+    if ($infile =~ /.fa$|.fasta$|.fna$/) {
+    $FH = Bio::SeqIO -> new(-file => "$infile", -format => 'Fasta');
+    }
+    elsif ($infile =~ /.gz$/) {
+    $FH = Bio::SeqIO -> new(-file => "bgzip -cd $infile| ", -format => 'Fasta');
+    }
+    elsif ($infile =~ /.bz2$/) {
+    $FH = Bio::SeqIO -> new(-file => "bzip2 -c $infile| ", -format => 'Fasta');
+    }
+    elsif ($infile =~ /.zip$/) {
+    $FH = Bio::SeqIO -> new(-file => "unzip -p $infile| ", -format => 'Fasta');
+    } else {
+	die ("$!: do not recognise file type $infile");
+	# if this happens, add the file type with correct opening proc
+    }
+    return $FH;
+}
+
